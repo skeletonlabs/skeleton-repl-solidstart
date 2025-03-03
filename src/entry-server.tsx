4a1a661518be404b089d59bbe389f1bf75327b60
endigo9740 <gundamx9740@gmail.com>
2025-03-03 14:04:04 -0600

    Install Tailwind

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -4,10 +4,11 @@ import { createHandler, StartServer } from "@solidjs/start/server";
 export default createHandler(() => (
   <StartServer
     document={({ assets, children, scripts }) => (
-      <html lang="en">
+      <html lang="en" data-theme="cerberus">
         <head>
           <meta charset="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <title>Skeleton + Solidstart Playground</title>
           <link rel="icon" href="/favicon.ico" />
           {assets}
         </head>
